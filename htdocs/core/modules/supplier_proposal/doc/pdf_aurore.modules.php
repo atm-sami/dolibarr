b548f7552187de8b0dc28b35a8e0b5ded97be9e3
Sami Filali <139965072+samifilali@users.noreply.github.com>
2024-07-16 22:11:22 +0200

    Revert "NEW : add option to add external billing contact on the supplier_prop…"
    This reverts commit 4aa5e4578a1eb3c14936a20ca9894ce5ad5e5ca9.

--- htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
+++ htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
@@ -1356,11 +1356,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 
 			// If CUSTOMER contact defined, we use it
 			$usecontact = false;
-			if (getDolGlobalInt('SUPPLIER_PROPOSAL_ADD_BILLING_CONTACT')) {
-				$arrayidcontact = $object->getIdContact('external', 'CUSTOMER');
-			} else {
-				$arrayidcontact = array_merge($object->getIdContact('external', 'CUSTOMER'), $object->getIdContact('external', 'BILLING'));
-			}
+			$arrayidcontact = $object->getIdContact('external', 'CUSTOMER');
 			if (count($arrayidcontact) > 0) {
 				$usecontact = true;
 				$result = $object->fetch_contact($arrayidcontact[0]);
